10abab9839070c75468e2591095f949339b249f4
Gurgen Baghdasaryan <gurgenbaghdasaryan@macbook-pro-de-gurgen.home>
2021-07-19 23:41:01 +0200

    title

--- asyncawait.js
+++ asyncawait.js
@@ -1,24 +1,28 @@
 
 
-let contenedor = document.getElementById('contenedor')
+let contenedor = document.getElementById('contenedor');
 let numList = Math.floor(Math.random()*150);
 
 async function getPokemon(){
 
     let pokeList = await fetch(`https://pokeapi.co/api/v2/pokemon/${numList}/`)
-    let data = await pokeList.json()
+    let pokeData = await pokeList.json();
 
     let dogList = await fetch(`https://dog.ceo/api/breeds/image/random`)
-    let dataDog = await dogList.json()
+    let dogData = await dogList.json();
 
-    contenedor.innerHTML = `
+    contenedor.innerHTML= `
 
-    <img src="${data.sprites.front_default}">
-    <h1>VS</h1>
-    <img src="${dataDog.message}">
-`
+        <img src="${pokeData.sprites.front_default}">
+        <h3>VERSUS</h3>
+        <img src="${dogData.message}">
+    
+    `;
 }
 getPokemon()
+
+
+
 
 
 
